c0e0365d911934f0c46ac7148f6a22d3635b3594
Adao <sansonov@gmail.com>
2020-06-07 16:04:33 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,97 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  getAllToys();
+  createToy();
 });
+
+const urlToys = 'http://localhost:3000/toys'
+
+function getAllToys() {
+return fetch(urlToys)
+.then(resp => resp.json())
+.then(toysArray => showToys(toysArray))
+.catch(error => console.log(error))};
+
+function showToys(toysArray) {
+  toysArray.forEach(toy => {
+    addDiv(toy);
+  });
+  
+}
+
+function createDiv(toy) {
+  const div = document.createElement('div');
+  div.className = "card";
+  const header = document.createElement('h2');
+  header.innerText = toy.name;
+  const img = document.createElement('img');
+  img.className = "toy-avatar"; 
+  img.src = toy.image;
+  const p = document.createElement('p');
+  p.innerText = `${toy.likes} Likes`;  
+  const button = document.createElement('button');
+  button.className = "like-btn";
+  button.innerText = "Like";
+  div.append(header);
+  div.append(img);
+  div.append(p);
+  div.append(button);
+
+  button.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let object = {
+    likes: toy.likes + 1
+    };
+    
+    let configObject = {
+    method: 'PATCH',
+    headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+    },
+    body: JSON.stringify(object)
+    };
+    e.target.parentNode.remove();
+
+    fetch(`${urlToys}/${toy.id}`, configObject)
+    .then(resp => resp.json())
+    .then(data => addDiv(data))
+    .catch(error => console.log(error));
+  });
+
+  return div
+}
+
+function addDiv(toy) {
+  const toyCollection = document.getElementById('toy-collection')
+  const div = createDiv(toy);
+  toyCollection.append(div);
+}
+
+function createToy() {
+  const form = document.querySelector('.add-toy-form')
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let toyObject = {
+    name: e.target[0].value,
+    image: e.target[1].value
+    };
+    
+    let configObject = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+    },
+    body: JSON.stringify(toyObject)
+    };
+    form.reset();
+
+    fetch(urlToys, configObject)
+    .then(resp => resp.json())
+    .then(data => addDiv(data))
+    .catch(error => console.log(error));
+  })
+}
